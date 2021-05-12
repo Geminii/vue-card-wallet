292efd0b62870ea01b62a286328ec48faeea4f03
Jimmy Jouanne <jimmy.jouanne@fullsix.com>
2021-05-12 14:16:02 +0200

    chore: init project

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins',
+      },
+      fontSize: {
+        xxs: '8px',
+      },
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        orange: 'var(--color-orange)',
+        'dark-purple': 'var(--color-purple)',
+        blue: 'var(--color-blue)',
+      },
+      boxShadow: {
+        card: '0px 4px 40px #8ca4d8',
+      },
+      gridTemplateColumns: {
+        'recent-payment': '200px 1fr 1fr 1fr 26px',
+        'card-payment': '1fr 324px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
